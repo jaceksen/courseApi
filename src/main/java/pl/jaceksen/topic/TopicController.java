58abe74685fe692c0d33fae14742c9dc0eb061aa
Jacek Seń <jaceksen@gmail.com>
2018-05-04 10:13:24 +0200

    Zwracana hardcoded lista jako JSON.

--- src/main/java/pl/jaceksen/topic/TopicController.java
+++ src/main/java/pl/jaceksen/topic/TopicController.java
@@ -1,5 +1,8 @@
 package pl.jaceksen.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 
 	@RequestMapping("/topics")
-	public String getAllTopics() {
-		return "All topics";
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				
+				new Topic("spring", "Spring Framework", "Spring description"),
+				new Topic("java", "Core Java", "Java description"),
+				new Topic("javascript", "JavaScript", "JavaScript description"),
+				new Topic("angular", "Angular", "Angular description"),
+				new Topic("android", "Android", "Android description"),
+				new Topic("tensorflow", "TensorFlow", "TensorFlow description"),
+				new Topic("ionic", "Ionic", "Ionic description"),
+				new Topic("ai", "AI", "AI description")
+				
+				);
 	}
 }
